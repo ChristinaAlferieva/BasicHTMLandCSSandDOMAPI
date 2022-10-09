775d55c1c16c444b627a9f1e1cf7042fc63e11fc
ChristinaAlferieva <C19401882@mytudublin.ie>
2022-10-09 14:57:52 +0100

    Completed the JavaScript

--- problem-2/site.js
+++ problem-2/site.js
@@ -1,92 +1,185 @@
-addNote = document.querySelector(".add_note"),
-popupNote = document.querySelector(".note_popup"),
-addButton = popupNOte.querySelector("button"),
-popupTitle = popupBox.querySelector("header p"),
-closeIcon = popupBox.querySelector("header i"),
-titleTag = popupBox.querySelector("input"),
-descTag = popupBox.querySelector("textarea");
+(function() {
+	'use strict';
 
-const notes = JSON.parse(localStorage.getItem("notes") || "[]");
-let isUpdate = false, updateID;
-addNote.addEventListener("click", () => {
-	popupTitle.innerText = "Add a note";
-	addButton.innerText = "Add note";
-	popupNote.classList.add("show");
-	document.querySelector("body").style.overflow = "hidden";
-	if(window.innerWidth > 700) titleTag.focus();
-});
+	var notesApp = {
+		noteDetails: document.getElementById('note_details'),
+		noteTitle: document.getElementById('note_title'),
+		title: document.getElementById('title'),
+		noteMessage: document.getElementById('body'),
+		colour: document.getElementById('colour'),
+		addButton: document.getElementById('add_button'),
+		errorMessage: document.getElementById('error'),
+		editButton: document.querySelector('.edit'),
+		deleteButton: document.querySelector('.delete'),
+		notes: document.getElementById('notes'),
+		myNotes: document.getElementById('my_notes'),
+		editMode: false,
 
-closeIcon.addEventListener("click", () => {
-	isUpdate = false;
-	titleTag.value = descTag.value = "";
-	popupNote.classList.remove("show");
-	document.querySelector("body").style.overflow = "auto";
-});
+		init: function() {
+			notesApp.title.addEventListener('focus', notesApp.clearError);
+			notesApp.noteMessage.addEventListener('focus', notesApp.clearError);
 
-function showNotes() {
-    if(!notes) return;
-    document.querySelectorAll(".note").forEach(li => li.remove());
-    notes.forEach((note, id) => {
-        let filterDesc = note.description.replaceAll("\n", '<br/>');
-        let liTag = `<li class="note">
-                        <div class="details">
-                            <p>${note.title}</p>
-                            <span>${filterDesc}</span>
-                        </div>
-                        <div class="bottom-content">
-                            <span>${note.date}</span>
-                            <div class="settings">
-                                <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
-                                <ul class="menu">
-                                    <li onclick="updateNote(${id}, '${note.title}', '${filterDesc}')"><i class="uil uil-pen"></i>Edit</li>
-                                    <li onclick="deleteNote(${id})"><i class="uil uil-trash"></i>Delete</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </li>`;
-        addNote.insertAdjacentHTML("afterend", liTag);
-    });
-}
+			notesApp.title.addEventListener('keypress', notesApp.detectInput);
+			notesApp.noteMessage.addEventListener('keypress', notesApp.detectInput);
 
-showNotes();
-function showMenu(elem) {
-    elem.parentElement.classList.add("show");
-    document.addEventListener("click", e => {
-        if(e.target.tagName != "I" || e.target != elem) {
-            elem.parentElement.classList.remove("show");
-        }
-    });
-}
-function deleteNote(noteId) {
-    let confirmDel = confirm("Are you sure you want to delete this note?");
-    if(!confirmDel) return;
-    notes.splice(noteId, 1);
-    localStorage.setItem("notes", JSON.stringify(notes));
-    showNotes();
-}
-function updateNote(noteId, title, filterDesc) {
-    let description = filterDesc.replaceAll('<br/>', '\r\n');
-    updateId = noteId;
-    isUpdate = true;
-    addNote.click();
-    titleTag.value = title;
-    descTag.value = description;
-    popupTitle.innerText = "Update a Note";
-    addButton.innerText = "Update Note";
-}
-addBtn.addEventListener("click", e => {
-    e.preventDefault();
-    let title = titleTag.value.trim(),
-    description = descTag.value.trim();
-    if(title || description) {
-        if(!isUpdate) {
-            notes.push(noteInfo);
-        } else {
-            isUpdate = false;
-            notes[updateId] = noteInfo;
-        }
-        localStorage.setItem("notes", JSON.stringify(notes));
-        showNotes();
-        closeIcon.click();
-    }
-});
+			notesApp.addButton.addEventListener('click', notesApp.createNote);
+		},
+		detectInput: function() {
+			if(!notesApp.title.value || !notesApp.message.value) {
+				return;
+			} else {
+				notesApp.addButton.innerText = 'Add Note';
+			}
+		},
+		clearError: function() {
+			notesApp.title.classList.remove('isEmpty');
+			notesApp.noteMessage.classList.remove('isEmpty');
+			notesApp.errorMessage.innerHTML = '';
+		},
+		createNote: function() {
+			if(!notesApp.title.value || !notesApp.noteMessage.value) {
+				if(!notesApp.title.value) {
+				notesApp.title.classList.add('isEmpty');  
+			}
+			if(!notesApp.noteMessage.value) {
+				notesApp.noteMessage.classList.add('isEmpty');
+			}
+			notesApp.errorMessage.innerHTML = '<span>* Values are missing</span>';
+				return;
+			} else {
+			var note = new Object();
+
+			note.title = notesApp.title.value;
+			note.noteMessage = notesApp.noteMessage.value;
+			note.colour = notesApp.colour.value;
+
+			notesApp.addNote(note);
+			}
+		},
+		addNote: function(note) {
+			var li = document.createElement('li'),
+			deleteButton = document.createElement('span'),
+			editButton = document.createElement('span'),
+			title = document.createElement('span'),
+			noteMessage = document.createElement('span'),
+			footer = document.createElement('footer');
+
+			deleteButton.className = 'delete';
+			deleteButton.innerHTML = '<i class="fa fa-trash-o"></i> Delete';
+			deleteButton.addEventListener('click', notesApp.deleteNote);
+
+			title.className = 'note_title';
+			title.innerHTML = note.title;
+
+			noteMessage.className = 'note_message';
+			noteMessage.innerHTML = note.noteMessage;
+
+			editButton.className = 'edit';
+			editButton.innerHTML = '<i class="fa fa-pencil-square-o"></i> Edit';
+			editButton.addEventListener('click', notesApp.editNote);
+
+			footer.appendChild(editButton);
+		  
+			li.className = note.colour;
+
+			li.appendChild(deleteButton);
+			li.appendChild(title);
+			li.appendChild(noteMessage);
+			li.appendChild(footer);
+
+			notesApp.notes.prepend(li);
+
+			notesApp.title.value = '';
+			notesApp.noteMessage.value = '';
+
+			if(!notesApp.editMode) {
+				notesApp.addButton.innerText = 'Add Note';
+			} else {
+				setTimeout(function() {
+					notesApp.addButton.innerText = 'Add Note';
+				}, 150);
+			}
+		},
+		editNote: function() {
+			var li,
+			title,
+			noteMessage,
+			colour,
+			note = new Object();
+
+			li = this.parentNode.parentNode;
+
+			for(var i = 0; i < li.childNodes.length; i++) {
+				if(li.childNodes[i].className === 'note_title') {
+					title = li.childNodes[i].innerText;
+				}
+			}
+
+			for(var i = 0; i < li.childNodes.length; i++) {
+				if(li.childNodes[i].className === 'note_message') {
+					noteMessage = li.childNodes[i].innerText;
+				}
+			}
+
+			colour = li.getAttribute('class');
+
+			note.title = title;
+			note.noteMessage = noteMessage;
+			note.colour = colour;
+		  
+			notesApp.openNote(note);
+
+			setTimeout(function() {
+				li.remove();
+			}, 150);
+		},
+		openNote: function(note) {
+			if(!notesApp.editMode) {
+				notesApp.noteDetails.classList.add('hide');
+				notesApp.myNotes.classList.add('hide');
+		  
+				setTimeout(function() {
+					notesApp.noteTitle.innerText = 'Edit Note';
+			  
+					notesApp.addButton.innerText = 'Update';
+					notesApp.addButton.removeEventListener('click', notesApp.createNote);
+					notesApp.addButton.addEventListener('click', notesApp.saveNote);
+
+					notesApp.title.value = note.title;
+					notesApp.noteMessage.value = note.noteMessage;
+					notesApp.colour.value = note.colour;
+
+					notesApp.noteDetails.classList.remove('hide');
+					notesApp.editMode = true;
+				}, 150);
+			} else {
+				return;
+			}  
+		},
+		saveNote: function() {
+			notesApp.createNote();
+
+			notesApp.noteDetails.classList.add('hide');
+			notesApp.myNotes.classList.add('hide');
+		
+			setTimeout(function() {
+				notesApp.noteTitle.innerText = 'Create a new note';
+
+				notesApp.addButton.removeEventListener('click', notesApp.saveNote);
+				notesApp.addButton.addEventListener('click', notesApp.createNote);
+
+				notesApp.title.value = '';
+				notesApp.noteMessage.value = '';
+
+				notesApp.myNotes.classList.remove('hide');
+				notesApp.noteDetails.classList.remove('hide');
+				notesApp.editMode = false;
+			}, 150);
+		},    
+		deleteNote: function() {
+			this.parentNode.remove();
+		}
+	};
+
+	notesApp.init();
+})();
